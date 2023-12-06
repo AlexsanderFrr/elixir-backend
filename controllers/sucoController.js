9834cf48213ee956348f4cf6f5544926bf6bb8c1
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2023-12-06 15:12:37 -0300

    add rota busca por id e title suco

--- controllers/sucoController.js
+++ controllers/sucoController.js
@@ -33,4 +33,82 @@ router.post('/add', upload.single('img1'), async (req, res) => {
     }
 });
 
+//Buscar todos os Sucos
+router.get('/all', async (req, res)=>{
+  try{
+    const suco = await Suco.findAll();
+
+    const sucosWithImagePaths = suco.map(suco =>{
+      if (suco.img1){
+        const imagePath = `/img/${suco.img1}`;
+        return {
+          ...suco.dataValues,
+          img1: imagePath,
+        };
+      }else{
+        return { ...suco.dataValues, img1: null};
+      }
+    });
+    res.status(200).json(sucosWithImagePaths);
+  } catch (error) {
+    console.error(error);
+    res.status(500).json({error: 'Erro interno no servidor'})
+  }
+});
+
+//Busca por id Suco 
+router.get('/:id', async (req, res) =>{
+  try{
+    const id = req.params.id;
+    const suco = await Suco.findByPk(id);
+
+    if (!suco) {
+      throw new Error('Receita não encontrado');
+    }
+
+    const imagePath = suco.img1 ? `/img/${suco.img1}` : null;
+    const sucosWithImagePaths = {
+      ...suco.dataValues,
+      img1: imagePath,
+    };
+
+    res.status(200).json(sucosWithImagePaths);
+  }catch (error){
+    res.status(400).json({error: error.message});
+  }
+});
+
+//Rota para busacr um Suco através do titulo
+router.get('/title', async (req, res)=>{
+  // Obtenha o título da consulta
+  const {title} = req.query; 
+
+  try{
+    const suco = await Suco.findAll({
+      where:{
+        nome: {
+          [Op.like]: `%${title}%`
+        }
+      }
+    });
+
+    const sucosWithImagePaths = suco.map(suco =>{
+      const imagePath = suco.img1 ? `/img/${suco.img1}` : null;
+      return{
+        ...suco.dataValues,
+        img1:imagePath,
+      };
+    });
+
+    if (sucosWithImagePaths.lenfth > 0){
+      res.json(sucosWithImagePaths);
+    }else{
+      throw new Error('Nenhuma receita de suco encontrada com o titulo fornecido');
+    }
+  }catch (error){
+    res.status(400).json({ error: error.message});
+  }
+});
+
+
 module.exports = router;
