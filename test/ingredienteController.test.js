64f18200f17ee9db761a607b88254889a9a0c545
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-06-25 21:50:46 -0300

    teste cad ingrediente

---
+++ test/ingredienteController.test.js
@@ -0,0 +1,35 @@
+const request = require('supertest');
+const express = require('express');
+const bodyParser = require('body-parser');
+const path = require('path');
+const ingredienteRouter = require('../controllers/ingredienteControler');
+const Ingrediente = require('../models').Ingrediente;
+
+jest.mock('../models');
+
+const app = express();
+app.use(bodyParser.json());
+app.use('/ingredientes', ingredienteRouter);
+
+describe('Testes para o Controlador de Ingredientes', () => {
+  beforeEach(() => {
+    delete require.cache[require.resolve('../controllers/ingredienteControler')];
+    jest.clearAllMocks();
+  });
+
+  it('Deve cadastrar um novo ingrediente', async () => {
+    const novoIngrediente = {
+      nome: 'Tomate',
+      beneficios: 'Rico em vitamina C',
+    };
+
+    Ingrediente.create.mockResolvedValue(novoIngrediente);
+
+    const response = await request(app)
+      .post('/ingredientes/add')
+      .send(novoIngrediente)
+      .expect(200);
+
+    expect(response.body.message).toBe('Ingrediente Cadastrado com sucesso');
+  });
+});
