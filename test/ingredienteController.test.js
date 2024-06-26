0a1d3f3db7960bbb46dedb0e6a1e214043a1066b
Rafael Oliveira <rafael.oliveira.sss@gmail.com>
2024-06-25 23:13:12 -0300

    teste buscar um e todos ingredientes

--- test/ingredienteController.test.js
+++ test/ingredienteController.test.js
@@ -34,7 +34,59 @@ describe('Testes para o Controlador de Ingredientes', () => {
         expect(response.body.message).toBe('Ingrediente Cadastrado com sucesso');
         done();
       });
-
-    
   });
+
+  it('Deve recuperar um ingrediente', async () => {
+    const idIngrediente = 1;
+    const mockIngrediente = {
+      id: idIngrediente,
+      nome: 'ingrediente Teste',
+      beneficios: 'Descricao Teste',
+      img: 'fakeimage.png'
+      
+    };
+
+    // Mock da função findByPk do modelo Diagnostico
+    Ingrediente.findByPk.mockResolvedValue(mockIngrediente);
+
+    request(app)
+      .get(`/ingrediente/${idIngrediente}`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done (err);
+        expect(res.body.id).toBe(idIngrediente);
+        expect(res.body.nome).toBe(mockIngrediente.nome);
+        expect(res.body.beneficios).toBe(mockIngrediente.beneficios);
+        expect(res.body.img).toBe(mockIngrediente.img);
+        done();
+      });
+  });
+
+  it('Deve listar todos os ingredientes', async () => {
+    const mockIngrediente = [
+      { id: 1, nome: 'Ingrediente Teste 1', beneficios: 'Beneficio Teste 1', img: 'fakeimage1.png'},
+      { id: 2, nome: 'Ingrediente Teste 2', beneficios: 'Beneficio Teste 2', img: 'fakeimage2.png'}
+    ];
+
+    // Mock da função findAll do modelo Ingrediente
+    Ingrediente.findAll.mockResolvedValue(mockIngrediente);
+
+    request(app)
+      .get(`/ingredientes/all`)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.length).toBe(2);
+        expect(res.body[0].id).toBe(1);
+        expect(res.body[0].nome).toBe('Ingrediente Teste 1');
+        expect(res.body[0].beneficios).toBe('Beneficio Teste 1');
+        expect(res.body[0].img).toBe('fakeimage1.png');
+        expect(res.body[1].id).toBe(2);
+        expect(res.body[1].nome).toBe('Ingrediente Teste 2');
+        expect(res.body[1].beneficios).toBe('Beneficio Teste 2');
+        expect(res.body[1].img).toBe('fakeimage2.png');
+        done();
+      });
+  });
+
 });
