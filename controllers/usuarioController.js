277f757bb2a9aa8abc1eb0e1d5d63ef84aafd0c2
Rafael Oliveira <rafael.oliveira.sss@gmail.com>
2024-06-28 21:01:07 -0300

    teste de token e token

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -1,23 +1,61 @@
 const express = require('express');
 const router = express.Router();
 const { Op } = require('sequelize');
-
+const bcrypt = require('bcrypt');
+const jwt = require('jsonwebtoken');
 
 const Usuario = require('../models').Usuario;
+
+const SECRET_KEY = 'your_secret_key'; // Use uma chave secreta segura e armazenada em variáveis de ambiente
 
 // Cadastra Usuario (POST)
 router.post('/add', async (req, res) => {
     try {
         const { nome, email, senha } = req.body;
-        const newUsuario = await Usuario.create({ nome, email, senha })
+        const hashedSenha = await bcrypt.hash(senha, 10); // Hash da senha
+        const newUsuario = await Usuario.create({ nome, email, senha: hashedSenha });
         res.status(200).json({ message: 'Usuario Cadastrado com sucesso', usuario: newUsuario });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
 });
 
+// Login (POST)
+router.post('/login', async (req, res) => {
+    try {
+        const { email, senha } = req.body;
+        const usuario = await Usuario.findOne({ where: { email } });
+        if (!usuario) {
+            return res.status(400).json({ message: 'Email ou senha inválidos' });
+        }
+
+        const isPasswordValid = await bcrypt.compare(senha, usuario.senha);
+        if (!isPasswordValid) {
+            return res.status(400).json({ message: 'Email ou senha inválidos' });
+        }
+
+        const token = jwt.sign({ id: usuario.id }, SECRET_KEY, { expiresIn: '1h' });
+        res.status(200).json({ message: 'Autenticado com sucesso', token });
+    } catch (error) {
+        res.status(400).json({ error: error.message });
+    }
+});
+
+// Middleware de autenticação
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
 // Buscar todos os Usuarios
-router.get('/all', async (req, res) => {
+router.get('/all', authenticateToken, async (req, res) => {
     try {
         const usuarios = await Usuario.findAll();
         res.status(200).json(usuarios);
@@ -26,10 +64,9 @@ router.get('/all', async (req, res) => {
     }
 });
 
-//Busca Por id do Usuario (GET)
-router.get('/:id', async (req, res) => {
+// Busca Por id do Usuario (GET)
+router.get('/:id', authenticateToken, async (req, res) => {
     try {
-        const id = req.params;
         const usuario = await Usuario.findByPk(req.params.id);
         res.status(200).json(usuario);
     } catch (error) {
@@ -37,15 +74,14 @@ router.get('/:id', async (req, res) => {
     }
 });
 
-//Alterar Usuario por ID (PUT)
-router.put('/:id', async (req, res) => {
+// Alterar Usuario por ID (PUT)
+router.put('/:id', authenticateToken, async (req, res) => {
     try {
         const { nome, email, senha } = req.body;
+        const hashedSenha = await bcrypt.hash(senha, 10);
         await Usuario.update(
-            { nome, email, senha },
-            {
-                where: { id: req.params.id },
-            }
+            { nome, email, senha: hashedSenha },
+            { where: { id: req.params.id } }
         );
         res.status(200).json({ message: 'Usuario Atualizado com sucesso' });
     } catch (error) {
@@ -53,15 +89,11 @@ router.put('/:id', async (req, res) => {
     }
 });
 
-//Deletar Usuario por id (DELETE)
-router.delete('/:id', async (req, res) => {
+// Deletar Usuario por id (DELETE)
+router.delete('/:id', authenticateToken, async (req, res) => {
     try {
-        await Usuario.destroy({
-            where: {
-                id: req.params.id,
-            },
-        });
-        res.status(200).json({ message: 'Usuario Excluído com sucesso' })
+        await Usuario.destroy({ where: { id: req.params.id } });
+        res.status(200).json({ message: 'Usuario Excluído com sucesso' });
     } catch (error) {
         res.status(400).json({ error: error.message });
     }
--- test/usuariocontroller.test.js
+++ test/usuariocontroller.test.js
@@ -1,6 +1,7 @@
 const request = require('supertest');
 const express = require('express');
 const bodyParser = require('body-parser');
+const bcrypt = require('bcrypt');
 const usuarioRouter = require('../controllers/usuarioController');
 const { Usuario } = require('../models');
 
@@ -33,10 +34,55 @@ describe('Testes para o Controlador de Usuarios', () => {
       .expect(200);
 
     expect(response.body.message).toBe('Usuario Cadastrado com sucesso');
-    //expect(response.body.Usuario).toEqual(mockUsuario);
+    //expect(response.body.usuario).toEqual(mockUsuario);
   });
 
-  it('Deve listar todos os usuários', async () => {
+  it('Deve fazer login e retornar um token JWT', async () => {
+    const loginUsuario = {
+      email: 'teste@teste.com',
+      senha: 'senha123'
+    };
+
+    const mockUsuario = {
+      id: 1,
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: await bcrypt.hash('senha123', 10) // Senha hasheada
+    };
+
+    Usuario.findOne.mockResolvedValue(mockUsuario);
+
+    const response = await request(app)
+      .post('/usuario/login')
+      .send(loginUsuario)
+      .expect(200);
+
+    expect(response.body.message).toBe('Autenticado com sucesso');
+    expect(response.body.token).toBeDefined();
+  });
+
+  it('Deve listar todos os usuários com autenticação', async () => {
+    const loginUsuario = {
+      email: 'teste@teste.com',
+      senha: 'senha123'
+    };
+
+    const mockUsuario = {
+      id: 1,
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: await bcrypt.hash('senha123', 10) // Senha hasheada
+    };
+
+    Usuario.findOne.mockResolvedValue(mockUsuario);
+
+    const loginResponse = await request(app)
+      .post('/usuario/login')
+      .send(loginUsuario)
+      .expect(200);
+
+    const token = loginResponse.body.token;
+
     const mockUsuarios = [
       { id: 1, nome: 'Teste 1', email: 'teste1@teste.com', senha: 'senha123' },
       { id: 2, nome: 'Teste 2', email: 'teste2@teste.com', senha: 'senha123' }
@@ -46,25 +92,69 @@ describe('Testes para o Controlador de Usuarios', () => {
 
     const response = await request(app)
       .get('/usuario/all')
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
 
     expect(response.body).toEqual(mockUsuarios);
   });
 
-  it('Deve buscar um usuário por ID', async () => {
-    const id = 1;
-    const mockUsuario = { id, nome: 'Teste', email: 'teste@teste.com', senha: 'senha123' };
+  it('Deve buscar um usuário por ID com autenticação', async () => {
+    const loginUsuario = {
+      email: 'teste@teste.com',
+      senha: 'senha123'
+    };
 
-    Usuario.findByPk.mockResolvedValue(mockUsuario);
+    const mockUsuario = {
+      id: 1,
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: await bcrypt.hash('senha123', 10) // Senha hasheada
+    };
+
+    Usuario.findOne.mockResolvedValue(mockUsuario);
+
+    const loginResponse = await request(app)
+      .post('/usuario/login')
+      .send(loginUsuario)
+      .expect(200);
+
+    const token = loginResponse.body.token;
+
+    const id = 1;
+    const mockUsuarioById = { id, nome: 'Teste', email: 'teste@teste.com', senha: 'senha123' };
+
+    Usuario.findByPk.mockResolvedValue(mockUsuarioById);
 
     const response = await request(app)
       .get(`/usuario/${id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
 
-    expect(response.body).toEqual(mockUsuario);
+    expect(response.body).toEqual(mockUsuarioById);
   });
 
-  it('Deve atualizar um usuário por ID', async () => {
+  it('Deve atualizar um usuário por ID com autenticação', async () => {
+    const loginUsuario = {
+      email: 'teste@teste.com',
+      senha: 'senha123'
+    };
+
+    const mockUsuario = {
+      id: 1,
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: await bcrypt.hash('senha123', 10) // Senha hasheada
+    };
+
+    Usuario.findOne.mockResolvedValue(mockUsuario);
+
+    const loginResponse = await request(app)
+      .post('/usuario/login')
+      .send(loginUsuario)
+      .expect(200);
+
+    const token = loginResponse.body.token;
+
     const id = 1;
     const updatedData = { nome: 'Teste Atualizado', email: 'atualizado@teste.com', senha: 'novaSenha123' };
 
@@ -72,21 +162,44 @@ describe('Testes para o Controlador de Usuarios', () => {
 
     const response = await request(app)
       .put(`/usuario/${id}`)
+      .set('Authorization', `Bearer ${token}`)
       .send(updatedData)
       .expect(200);
 
     expect(response.body.message).toBe('Usuario Atualizado com sucesso');
   });
 
-  it('Deve excluir um usuário por ID', async () => {
+  it('Deve excluir um usuário por ID com autenticação', async () => {
+    const loginUsuario = {
+      email: 'teste@teste.com',
+      senha: 'senha123'
+    };
+
+    const mockUsuario = {
+      id: 1,
+      nome: 'Teste',
+      email: 'teste@teste.com',
+      senha: await bcrypt.hash('senha123', 10) // Senha hasheada
+    };
+
+    Usuario.findOne.mockResolvedValue(mockUsuario);
+
+    const loginResponse = await request(app)
+      .post('/usuario/login')
+      .send(loginUsuario)
+      .expect(200);
+
+    const token = loginResponse.body.token;
+
     const id = 1;
 
     Usuario.destroy.mockResolvedValue(1);
 
     const response = await request(app)
       .delete(`/usuario/${id}`)
+      .set('Authorization', `Bearer ${token}`)
       .expect(200);
 
     expect(response.body.message).toBe('Usuario Excluído com sucesso');
   });
- });
+});
