ec62f50627e362b92967937ae6a56bddf49f98d4
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-07-01 19:50:18 -0300

    add authMiddleware.js

---
+++ controllers/middlewares/authMiddleware.js
@@ -0,0 +1,17 @@
+const jwt = require('jsonwebtoken');
+ // Use uma chave secreta segura e armazenada em variáveis de ambiente
+const SECRET_KEY = 'your_secret_key';
+
+const authenticateToken = (req, res, next) => {
+    const authHeader = req.headers['authorization'];
+    const token = authHeader && authHeader.split(' ')[1];
+    if (token == null) return res.sendStatus(401);
+
+    jwt.verify(token, SECRET_KEY, (err, user) => {
+        if (err) return res.sendStatus(403);
+        req.user = user;
+        next();
+    });
+};
+
+module.exports = authenticateToken;
