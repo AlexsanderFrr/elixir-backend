7e3c9860555344cd5074b15c0b952b629049ade6
Alexsander Ferreira <ferreiraalexsander3105@gmail.com>
2024-07-08 22:14:01 -0300

    update rotas usuarios /me

--- controllers/usuarioController.js
+++ controllers/usuarioController.js
@@ -4,20 +4,18 @@ const router = express.Router();
 const { Op } = require('sequelize');
 const bcrypt = require('bcrypt');
 const jwt = require('jsonwebtoken');
-const passport = require('../middlewares/passportConfig');
+//const passport = require('../middlewares/passportConfig');
 const Usuario = require('../models').Usuario;
 //const { SECRET_KEY } = process.env;
 const SECRET_KEY = process.env.SECRET_KEY;
-const authenticateToken = passport.authenticate('jwt', { session: false });
-//const authenticateToken = require('../middlewares/authMiddleware');
-
-//const SECRET_KEY = 'your_secret_key'; // Use uma chave secreta segura e armazenada em variáveis de ambiente
+//const authenticateToken = passport.authenticate('jwt', { session: false });
+const authenticateToken = require('../middlewares/authMiddleware');
 
 // Cadastra Usuario (POST)
 router.post('/add', async (req, res) => {
     try {
         const { nome, email, senha } = req.body;
-        const hashedSenha = await bcrypt.hash(senha, 10); // Hash da senha
+        const hashedSenha = await bcrypt.hash(senha, 10);
         const newUsuario = await Usuario.create({ nome, email, senha });
         res.status(200).json({ message: 'Usuario Cadastrado com sucesso', usuario: newUsuario });
     } catch (error) {
@@ -30,13 +28,15 @@ router.post('/login', async (req, res) => {
     try {
         const { email, senha } = req.body;
         const usuario = await Usuario.findOne({ where: { email } });
+
         if (!usuario) {
-            return res.status(400).json({ message: 'Email ou senha inválidos' });
+            return res.status(400).json({ message: 'Email inválido' });
         }
 
         const isPasswordValid = await bcrypt.compare(senha, usuario.senha);
+
         if (!isPasswordValid) {
-            return res.status(400).json({ message: 'Email ou senha inválidos' });
+            return res.status(400).json({ message: 'Senha inválida' });
         }
 
         const token = jwt.sign({ id: usuario.id }, SECRET_KEY, { expiresIn: '1h' });
@@ -57,10 +57,14 @@ router.get('/all', authenticateToken, async (req, res) => {
     }
 });
 
-// Busca Por id do Usuario (GET)
-router.get('/:id', authenticateToken, async (req, res) => {
+
+// Busca informações do usuário autenticado (GET)
+router.get('/me', authenticateToken, async (req, res) => {
     try {
-        const usuario = await Usuario.findByPk(req.params.id);
+        const usuario = await Usuario.findByPk(req.user.id);
+        if (!usuario) {
+            return res.status(404).json({ message: 'Usuário não encontrado' });
+        }
         res.status(200).json(usuario);
     } catch (error) {
         res.status(400).json({ error: error.message });
@@ -68,24 +72,24 @@ router.get('/:id', authenticateToken, async (req, res) => {
 });
 
 // Alterar Usuario por ID (PUT)
-router.put('/:id', authenticateToken, async (req, res) => {
-    try {
-        const { nome, email, senha } = req.body;
-        const hashedSenha = await bcrypt.hash(senha, 10);
-        await Usuario.update(
-            { nome, email, senha: hashedSenha },
-            { where: { id: req.params.id } }
-        );
-        res.status(200).json({ message: 'Usuario Atualizado com sucesso' });
-    } catch (error) {
-        res.status(400).json({ error: error.message });
-    }
-});
+ router.put('/me', authenticateToken, async (req, res) => {
+     try {
+         const { nome, email, senha } = req.body;
+         const hashedSenha = await bcrypt.hash(senha, 10);
+         await Usuario.update(
+             { nome, email, senha: hashedSenha },
+             { where: { id: req.user.id } }
+         );
+         res.status(200).json({ message: 'Usuario Atualizado com sucesso' });
+     } catch (error) {
+         res.status(400).json({ error: error.message });
+     }
+ });
 
 // Deletar Usuario por id (DELETE)
-router.delete('/:id', authenticateToken, async (req, res) => {
+router.delete('/me', authenticateToken, async (req, res) => {
     try {
-        await Usuario.destroy({ where: { id: req.params.id } });
+        await Usuario.destroy({ where: { id: req.user.id } });
         res.status(200).json({ message: 'Usuario Excluído com sucesso' });
     } catch (error) {
         res.status(400).json({ error: error.message });
