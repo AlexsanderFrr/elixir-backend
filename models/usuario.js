0126df800301d773d6359792a2c1d33fba03c554
CarlosEduardoBelo <carlos.eduardo.belo@hotmail.com>
2024-07-12 21:10:11 -0300

    Role atribuido ao usuário

--- models/usuario.js
+++ models/usuario.js
@@ -5,7 +5,7 @@ const { Model } = require('sequelize');
 module.exports = (sequelize, DataTypes) => {
   class Usuario extends Model {
     static associate(models) {
-      // Defina as associações aqui, se houver
+      // define association here
     }
   }
 
@@ -15,14 +15,15 @@ module.exports = (sequelize, DataTypes) => {
     senha: {
       type: DataTypes.STRING,
       set(value) {
-        const hashedPwd = bcrypt.hashSync(value, bcrypt.genSaltSync(10));
-        this.setDataValue('senha', hashedPwd);
+        const hashedPwd = bcrypt.hashSync(value,
+          bcrypt.genSaltSync(10));
+          this.setDataValue('senha', hashedPwd);
       },
     },
+    role: DataTypes.STRING
   }, {
     sequelize,
     modelName: 'Usuario',
   });
-
   return Usuario;
 };
