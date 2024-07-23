7ef9336711018da9d8533355ff84d6bf32d28199
Rafael Oliveira <rafael.oliveira.sss@gmail.com>
2024-07-22 21:41:18 -0300

    teste busca allInformation

--- test/allinformation.test.js
+++ test/allinformation.test.js
@@ -42,5 +42,55 @@ describe('Testes para o Controlador de AllInformations', () => {
         expect(res.body.message).toBe('Informações Cadastradas com sucesso');
         done();
       });
+
+
+  });
+
+  // Teste para listar todas as informações
+  it('Deve listar todas as informações', async () => {
+    const mockAllInformations = [
+      { id: 1, fk_suco: 1, fk_diagnostico: 1, fk_ingredientes: [1, 2] },
+      { id: 2, fk_suco: 2, fk_diagnostico: 2, fk_ingredientes: [2, 3] },
+    ];
+
+    // Mock da função findAll do modelo AllInformations
+    AllInformation.findAll.mockResolvedValue(mockAllInformations);
+
+    // Chama a rota de listar todas as informações
+    request(app)
+      .get('/allinformation/all')
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.length).toBe(2);
+        expect(res.body[0].id).toBe(1);
+        expect(res.body[1].id).toBe(2);
+        done();
+      });
+  });
+
+  // Teste para atualizar informações
+  it('Deve atualizar informações', async () => {
+    const idAllInformation = 1;
+    const informacaoAtualizada = {
+      fk_suco: 2,
+      fk_diagnostico: 2,
+      fk_ingredientes: [2, 3],
+    };
+
+    // Mock da função findByPk e save do modelo AllInformations
+    AllInformations.findByPk.mockResolvedValue(informacaoAtualizada);
+    AllInformations.save.mockResolvedValue({ ...informacaoAtualizada, id: idAllInformation });
+
+    // Chama a rota de atualizar informações
+    request(app)
+      .put(`/allinformation/update/${idAllInformation}`)
+      .send(informacaoAtualizada)
+      .expect(200)
+      .end((err, res) => {
+        if (err) return done(err);
+        expect(res.body.message).toBe('Informações atualizadas com sucesso');
+        done();
+      });
   });
 });
